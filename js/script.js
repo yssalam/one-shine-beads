019098240eab8aba1f3e2d263dfeb194c0047b16
Yoga Subhi Salam <yogasubhisalam79@gmail.com>
2026-02-04 17:18:32 +0700

    update

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 //Toggle active
 const navbarNav = document.querySelector(".navbar-nav");
 const searchForm = document.querySelector(".search-form");
+const cartForm = document.querySelector(".shopping-cart");
 
 //Seacrh langsung bisa ketik
 const searchBox = document.querySelector("#search-box");
@@ -9,6 +10,12 @@ const searchBox = document.querySelector("#search-box");
 document.querySelector("#hamburger-menu").onclick = (e) => {
   e.preventDefault();
   navbarNav.classList.toggle("active");
+};
+
+//ketika cart di klik
+document.querySelector("#shopping-cart").onclick = (e) => {
+  e.preventDefault();
+  cartForm.classList.toggle("active");
 };
 
 //ketika seacrh di klik
@@ -21,6 +28,7 @@ document.querySelector("#search").onclick = (e) => {
 //klik di luar element
 const hamburgerOut = document.querySelector("#hamburger-menu");
 const seacrhOut = document.querySelector("#search");
+const cartOut = document.querySelector("#shopping-cart");
 
 document.addEventListener("click", function (e) {
   if (!hamburgerOut.contains(e.target) && !navbarNav.contains(e.target)) {
@@ -28,5 +36,8 @@ document.addEventListener("click", function (e) {
   }
   if (!seacrhOut.contains(e.target) && !searchForm.contains(e.target)) {
     searchForm.classList.remove("active");
+  }
+  if (!cartOut.contains(e.target) && !cartForm.contains(e.target)) {
+    cartForm.classList.remove("active");
   }
 });
